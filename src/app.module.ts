21a696bdec619a53523b5d38371fe659e13c1157
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-02-11 20:17:08 +0400

    Add project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [AuthModule, CategoryModule, ProductModule, ReviewModule, OrderModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
