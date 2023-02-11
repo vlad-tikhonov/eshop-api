39f46b4d3ccf35107bfe4b82165f9f9aab0036bf
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-02-12 00:29:02 +0400

    Fix review model

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,6 +1,17 @@
-export class ReviewModel {
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+	@prop()
 	name: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	description: string;
+
+	@prop()
 	rating: number;
 }
