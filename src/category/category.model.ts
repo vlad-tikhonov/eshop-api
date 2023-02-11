5022306e925b62d79ef4c08ae6ebca6f2d7af8aa
Vladislav Tikhonov <vladislav000001@gmail.com>
2023-02-12 00:31:34 +0400

    Fix category model

--- src/category/category.model.ts
+++ src/category/category.model.ts
@@ -1,4 +1,11 @@
-export class CategoryModel {
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface CategoryModel extends Base {}
+export class CategoryModel extends TimeStamps {
+	@prop()
 	image: string;
+
+	@prop()
 	title: string;
 }
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { CategoryController } from './category.controller';
+import { CategoryModel } from './category.model';
 
 @Module({
-  controllers: [CategoryController]
+	controllers: [CategoryController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: CategoryModel,
+				schemaOptions: {
+					collection: 'Category',
+				},
+			},
+		]),
+	],
 })
 export class CategoryModule {}
